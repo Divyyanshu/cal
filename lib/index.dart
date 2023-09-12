ac6235b42d3303a06e855bab49de37af257db064
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-12 12:07:34 +0530

    sec commit

--- lib/index.dart
+++ lib/index.dart
@@ -1,3 +1,9 @@
+// Electric bill calculator app 
+//  this  app build by divyanshu tailor : github id : Divyyanshu
+//  CHECK MY DEPENCENCIES IN pubspec.yaml 
+//   and this is my first flutter app 
+
+
 import 'package:calculator/screen.dart';
 import 'package:flutter/material.dart';
 
